aafc859bde16e00acf4eed9e18fdc53a8f5190da
Peter Moser <peterjamoser@bluewin.ch>
2021-02-09 08:08:49 +0100

    9.2. added data in separate file from previous years

--- Other_Weather.R
+++ Other_Weather.R
@@ -5,11 +5,25 @@ library(reshape2)
 #get keyfile with units and variables
 climvars<-read.table("climate_vars_recodings.csv", sep=",", fileEncoding = "UTF-8", header=T)
 
-# Download data
-urlfile="https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_SMA_current.csv"
+# Download data current year
+urlfilecur="https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_SMA_current.csv"
+
+# Download data previous years
+urlfileprev="https://data.geo.admin.ch/ch.meteoschweiz.klima/nbcn-tageswerte/nbcn-daily_SMA_previous.csv"
+
+
 ################################
 # Format data according to data structure specification
-smaweather<-data.frame(read.table(url(urlfile), sep=";", header=T))
+smaweathercur<-data.frame(read.table(url(urlfilecur), sep=";", header=T))
+
+################################
+# Format data according to data structure specification
+smaweatherprev<-data.frame(read.table(url(urlfileprev), sep=";", header=T))
+
+smaweather<-rbind(smaweatherprev, smaweathercur)
+
+
+
 smaweather$date<-as.Date(as.character(smaweather$date), format="%Y%m%d")
 smaweather$rre150d0<-as.numeric(smaweather$rre150d0)
 smaweather$rre150d0<-ifelse(is.na(smaweather$rre150d0), 0, smaweather$rre150d0)
@@ -35,6 +49,8 @@ weatherzh<-droplevels(data.frame(date=smaweather$date,
 
 
 # export result
+#Cut off deep past
+weatherzh<-subset(weatherzh, date>"2017-12-31")
 
 write.table(weatherzh, "Other_Weather.csv", sep=",", fileEncoding="UTF-8", row.names = F)
 
